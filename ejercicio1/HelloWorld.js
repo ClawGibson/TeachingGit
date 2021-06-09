8ffc3c9ed6978513dd2d9f2076e4907dec496096
Gamaliel Bernal <gamalielbj@gmail.com>
2021-06-08 19:38:57 -0500

    Update HelloWorld.js

--- ejercicio1/HelloWorld.js
+++ ejercicio1/HelloWorld.js
@@ -1,7 +1,8 @@
-<<<<<<< HEAD
-tas
-=======
+
 console.log('Hola mundo')
 
 console.log('Hola mundo soy Gama')
->>>>>>> parent of bc964a9 (Update HelloWorld.js)
+
+console.log('Hola mundo soy Val :3')
+
+console.log('Hola mundo soy Ivan')
